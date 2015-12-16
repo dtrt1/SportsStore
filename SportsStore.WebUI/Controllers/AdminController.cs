e6d31c1dc5348571f6a77e3d407ed108a3de36d7
dtrt1 <psyfiiend@gmail.com>
2015-12-16 20:28:26 +0300

    step 12.1 - upload imgs - final

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using System.Web;
 using System.Web.Services.Description;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
@@ -29,10 +30,16 @@ namespace SportsStore.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product)
+        public ActionResult Edit(Product product, HttpPostedFileBase image = null)
         {
             if (ModelState.IsValid)
             {
+                if (image != null)
+                {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
                 repository.SaveProduct(product);
                 TempData["Message"] = string.Format("{0} has been saved", product.Name);
                 return RedirectToAction("Index");
